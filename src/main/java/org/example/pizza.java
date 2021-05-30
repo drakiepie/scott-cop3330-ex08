a2e48bc905067ffdac7496220ba34655e8a4c7cc
Drake Scott <drakescott999@hotmail.com>
2021-05-29 21:50:44 -0400

    added comments

--- src/main/java/org/example/pizza.java
+++ src/main/java/org/example/pizza.java
@@ -12,14 +12,17 @@ public class pizza {
         int numPizza = input.nextInt();
         System.out.println(numPeople + " people with " + numPizza + " pizzas.");
 
+        //assuming 8 slices per pizza + initializing variables for amount of slices everybody gets and remainder.
         int totalSlice = 8 * numPizza;
         int pizzaHaul, leftover;
 
+        //if there is no remainder, the leftover will be 0.
         if (totalSlice % numPeople == 0){
             leftover = 0;
             pizzaHaul = totalSlice / numPeople;
             System.out.println("Each person gets " + pizzaHaul + " pieces of pizza.\nThere are " + leftover + " leftover pieces.");
 
+        //assuming there is a remainder.
         } else {
             pizzaHaul = totalSlice / numPeople;
             leftover =  totalSlice % numPeople;
